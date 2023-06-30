ec638ff5bb2071da6f93edc9736a266fd28d7aa1
sema <semenpermogorov@gmail.com>
2023-06-30 16:52:34 +0300

    first version

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-# This is a sample Python script.
+import telebot
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+botTimeWeb = telebot.TeleBot('6323243276:AAFYCmIuRfV3b8U83N4H1oqqdEHZ5-Wc_Do')
+
+from telebot import types
+
+@botTimeWeb.message_handler(commands=['start'])
+def startBot(message):
+  first_mess = f"<b>{message.from_user.first_name} {message.from_user.last_name}</b>, привет!\nВсем на тебя похуй"
+  markup = types.InlineKeyboardMarkup()
+  button_yes = types.InlineKeyboardButton(text = 'Да', callback_data='yes')
+  markup.add(button_yes)
+  botTimeWeb.send_message(message.chat.id, first_mess, parse_mode='html', reply_markup=markup)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@botTimeWeb.callback_query_handler(func=lambda call:True)
+def response(function_call):
+  if function_call.message:
+     if function_call.data == "yes":
+        second_mess = "Это ебать бот типо который для пд можно выбрать задачу или иди нахуй короче"
+        markup = types.InlineKeyboardMarkup()
+        markup.add(types.InlineKeyboardButton("Пойти на хуй?", url="http://go-friend-go.narod.ru/"))
+        botTimeWeb.send_message(function_call.message.chat.id, second_mess, reply_markup=markup)
+        botTimeWeb.answer_callback_query(function_call.id)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+botTimeWeb.infinity_polling()
