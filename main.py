4c4f9c63b4c5a2c4638776502b34a7460e84d667
repa007 <50541383+repa007@users.noreply.github.com>
2023-07-02 19:06:03 +0300

    hui

--- main.py
+++ main.py
@@ -1,27 +1,47 @@
 import telebot
-
-botTimeWeb = telebot.TeleBot('6323243276:AAFYCmIuRfV3b8U83N4H1oqqdEHZ5-Wc_Do')
-
+bot = telebot.TeleBot('6323243276:AAFYCmIuRfV3b8U83N4H1oqqdEHZ5-Wc_Do')
 from telebot import types
 
-@botTimeWeb.message_handler(commands=['start'])
-def startBot(message):
-  first_mess = f"<b>{message.from_user.first_name} {message.from_user.last_name}</b>, привет!\nВсем на тебя похуй"
-  markup = types.InlineKeyboardMarkup()
-  button_yes = types.InlineKeyboardButton(text = 'Да', callback_data='yes')
-  markup.add(button_yes)
-  botTimeWeb.send_message(message.chat.id, first_mess, parse_mode='html', reply_markup=markup)
 
-#FFFFFFFF
-@botTimeWeb.callback_query_handler(func=lambda call:True)
+#Commands
+@bot.message_handler(commands=['start'])
+def startBot(message):
+  reply = f"<b>{message.from_user.first_name} {message.from_user.last_name}</b>, привет!\nЭто бот для выдачи задач. Для начала следует пройти регистрацию."
+  markup = types.InlineKeyboardMarkup()
+  button_registration = types.InlineKeyboardButton(text = 'Регистрация', callback_data='registration')
+  markup.add(button_registration)
+  bot.send_message(message.chat.id, reply, parse_mode='html', reply_markup=markup)
+
+@bot.message_handler(commands=['start'])
+def startBot(message):
+    reply = f"<b>{message.from_user.first_name} {message.from_user.last_name}</b>, привет!\nЭто бот для выдачи задач. Для начала следует пройти регистрацию."
+    markup = types.InlineKeyboardMarkup()
+    button_registration = types.InlineKeyboardButton(text='Регистрация', callback_data='registration')
+    markup.add(button_registration)
+    bot.send_message(message.chat.id, reply, parse_mode='html', reply_markup=markup)
+
+@bot.message_handler(commands=['GetExcel'])
+
+@bot.message_handler()#tol'ko vnizu
+def info(message):
+    bot.reply_to(message, "Я тебя не понимать 「(°ヘ°) \n Используй стандартные команды бота")
+
+#That is all Commands
+
+
+
+#Callback
+@bot.callback_query_handler(func=lambda call:True)
 def response(function_call):
   if function_call.message:
-     if function_call.data == "yes":
-        second_mess = "Это ебать бот типо который для пд можно выбрать задачу или иди нахуй короче"
+     if function_call.data == "registration":
+        reply = "Это ебать бот типо который для пд можно выбрать задачу или иди нахуй короче"
         markup = types.InlineKeyboardMarkup()
         markup.add(types.InlineKeyboardButton("Пойти на хуй?", url="http://go-friend-go.narod.ru/"))
-        botTimeWeb.send_message(function_call.message.chat.id, second_mess, reply_markup=markup)
-        botTimeWeb.answer_callback_query(function_call.id)
+        bot.send_message(function_call.message.chat.id, reply, reply_markup=markup)
+        bot.answer_callback_query(function_call.id)
 
 
-botTimeWeb.infinity_polling()
+
+
+bot.infinity_polling() #For permonent working
