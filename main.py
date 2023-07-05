e1b84f719d2824701ea1660324400d298661ccfc
repa007 <50541383+repa007@users.noreply.github.com>
2023-07-05 19:14:32 +0300

    bla

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pandas as pd
 from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
 from database import DB
 #mydb = DB("127.0.0.1",3306,"root","228228228Nm","mydb")#дима
-mydb = DB("127.0.0.1",3306,"root","j58AEiPY12@5","mydb")#Локальный
+mydb = DB("127.0.0.1",3306,"root","228228228Nm","mydb")#Локальный
 
 bot = telebot.TeleBot('6323243276:AAFYCmIuRfV3b8U83N4H1oqqdEHZ5-Wc_Do')
 from telebot import types
@@ -35,7 +35,13 @@ def help(message):
     markup.add(button_registration)
     bot.send_message(message.chat.id, reply, parse_mode='html', reply_markup=markup)
 
-@bot.message_handler(commands=['GetTask'])
+@bot.message_handler(commands=['Huy']) #для отладки
+def Huy(message):
+    reply,e = mydb.GetTasksAll()
+    reply = str(reply)
+    bot.send_message(message.chat.id, reply, parse_mode='html')
+
+@bot.message_handler(commands=['GetTasks'])
 def GetTasks(message):
     reply = 'Вот список задач, нажмите на интересующую чтобы узнать подробнее:'
     rec = mydb.GetTasksAll()
